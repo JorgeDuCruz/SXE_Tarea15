bd5b7a56a5ae5661d3f161ed7a225995fe98045b
jorge <jdurancruz@danielcastelao.org>
2025-12-01 11:49:49 +0100

    Inicio de la tarea

---
+++ extra-addons/cafe_al_fallo/controllers/controllers.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# from odoo import http
+
+
+# class CafeAlFallo(http.Controller):
+#     @http.route('/cafe_al_fallo/cafe_al_fallo', auth='public')
+#     def index(self, **kw):
+#         return "Hello, world"
+
+#     @http.route('/cafe_al_fallo/cafe_al_fallo/objects', auth='public')
+#     def list(self, **kw):
+#         return http.request.render('cafe_al_fallo.listing', {
+#             'root': '/cafe_al_fallo/cafe_al_fallo',
+#             'objects': http.request.env['cafe_al_fallo.cafe_al_fallo'].search([]),
+#         })
+
+#     @http.route('/cafe_al_fallo/cafe_al_fallo/objects/<model("cafe_al_fallo.cafe_al_fallo"):obj>', auth='public')
+#     def object(self, obj, **kw):
+#         return http.request.render('cafe_al_fallo.object', {
+#             'object': obj
+#         })
+
