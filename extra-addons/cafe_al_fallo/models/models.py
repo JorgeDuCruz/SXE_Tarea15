251ca1651ae857e40a35616b78b9034c81f3ad7c
jorge <jdurancruz@danielcastelao.org>
2025-12-01 12:39:58 +0100

    El modulo funciona, pero falta limitar que el rango de sueño sea de 1 a 10

--- extra-addons/cafe_al_fallo/models/models.py
+++ extra-addons/cafe_al_fallo/models/models.py
@@ -1,19 +1,34 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class cafe_al_fallo(models.Model):
-#     _name = 'cafe_al_fallo.cafe_al_fallo'
-#     _description = 'cafe_al_fallo.cafe_al_fallo'
+class cafe_al_fallo(models.Model):
+    _name = 'cafe_al_fallo.cafe_al_fallo'
+    _description = 'Lista del nivel de sueno del alumno y su bebida recomendada'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    nombre = fields.Char()
+    nivel_sueno = fields.Integer(
+        string='Nivel de Sueno (1-10)',
+        required=True,
+        default=1,
+        help="Indica qué tan dormido está el alumno, de 1 (poco) a 10 (máximo).",
+    )
+    bebida_recomendada = fields.Char(compute="_calcularBebida", store=True)
+
+    @api.depends('nivel_sueno')
+    def _calcularBebida(self):
+        for record in self:
+            nivel = record.nivel_sueno
+
+            if nivel >= 1 and nivel <= 3:
+                record.bebida_recomendada = "Café con leche"
+            elif nivel >= 4 and nivel <= 6:
+                record.bebida_recomendada = "Café solo largo"
+            elif nivel >= 7 and nivel <= 9:
+                record.bebida_recomendada = "Café solo larguísimo"
+            elif nivel == 10:
+                record.bebida_recomendada = "Inyección de adrenalina (¡Máximo Sueno!)"
+            else:
+                record.bebida_recomendada = "Nivel de sueno fuera del rango (1-10)"
 
